934633bb42552a94dcd860fc614caeeb5a4c9cb3
Peizhi Zheng <parisa1206@gmail.com>
2020-02-02 15:33:29 -0800

    cha5

--- cha5/exercises.js
+++ cha5/exercises.js
@@ -2,3 +2,40 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 console.log(arrays.reduce((a, b) => a.concat(b)));
+
+// Your own loop
+function loop(value,test,update,body){
+	if(test(value)){
+      body(value)
+      value=update(value)
+      }else return;
+  	return loop(value,test,update,body)
+}
+  
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
+
+//everything
+function every(array, test) {
+    for (let arr of array){
+      if(!test(arr)){
+          return false
+      }
+  }
+    return true
+}
+
+function every1(array, test) {
+    if(array.some(arr => !test(arr))) return false
+    return true;
+
+}
+
+console.log(every1([1, 3, 5], n => n < 10));
+// → true
+console.log(every1([2, 4, 16], n => n < 10));
+// → false
+console.log(every1([], n => n < 10));
+// → true
