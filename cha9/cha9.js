0bfbf6de66b27505cd63e53fa86d4d70d41d204e
Peizhi Zheng <parisa1206@gmail.com>
2020-02-06 22:00:16 -0800

    update

--- cha9/cha9.js
+++ cha9/cha9.js
@@ -110,3 +110,32 @@ while (match2 = numbers.exec(input)){
 }
 
 //Parsing an INI file
+function parseINI(string){
+    //Start with an object to hold the top-level fields
+    let result = {};
+    let section = result;
+    string.split(/\r?\n/).forEach(line => {
+        let match;
+        if (match =line.match(/^(\w+)=(.*)$/)){
+            section[match[1]]=match[2];
+        } else if (match = line.match(/\[(.*)\]$/)){
+            section = result[match[1]] ={};
+        } else if (!/^\s*(;.*)?$/.test(line)) {
+            throw new Error("Line '" + line + "' is not valid.");
+        }
+    })
+    return result;
+    
+}
+
+//recurring use of ^ and $ to make sure the expression matches the whole line, not just part of it
+console.log(parseINI(`
+name=Vasilis
+[address]
+city=Tessaloniki
+nknk`
+));
+
+let line = "fullname=Larry Doe"
+
+console.log(line.match(/^(\w+)=(.*)$/))
