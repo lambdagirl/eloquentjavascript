cd1234de12e68403839937bb5cc5d27bbfcad52d
Peizhi Zheng <parisa1206@gmail.com>
2020-02-12 22:22:27 -0800

    cha16

--- cha16/cha16.js
+++ cha16/cha16.js
@@ -224,5 +224,158 @@ level.prototype.touches = function(pos, size, type){
 State.prototype.update = function(time, keys){
     let actors = this.actors.map(actor => 
         actor.update(time, this, keys));
-    
+    let newState = new State(this.level, actors, this.status);
+
+    if (newState.status != "playing") return newState;
+
+    let player = newState.player;
+    if (this.level.touches(player.pos, player.size, "lava")){
+        return new State(this.level, actors, "lost");
+    }   
+    for (let actor of actors) {
+        if (actor != player && overlap(actor,player)){
+            newState = actor.collide(newState);
+        }
+    }
+    return newState; 
+}
+
+//when they overlap both along the x-axis and along the y-axis.
+function overlap(actor1, actor2) {
+    return (actor1.pos.x + actor1.size.x) > actor2.pos.x &&
+            actor1.pos.x < (actor2.pos.x + actor2.size.x)&&
+           (actor1.pos.y + actor2.size.y) > actor2.pos.y &&
+            actor1.pos.y < actor2.size.y + actor2.size.y;
+}
+
+Lava.prototype.collide = function(state) {
+    return new State(state.level, state.actors, "lost");
+};
+Coin.prototype.collide = function(state){
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    //when they are the last coin of the level;
+    if (!filtered.some(a => a.type =="coin")){
+        status = "won";
+    }
+    return new State(state.level, filtered, status);
+}
+
+//Actor Updates (time, state, keys)
+
+//Lava Updates
+Lava.prototype.update = function(time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+    //If no obstacle blocks that new position, it moves there.
+    if(!state.level.touches(newPos, this.size, "wall")){
+        return new Lava(newPos,this.speed, this.reset);
+    } else if (this.reset){
+    //dripping lava has a reset position, to which it jumps back when it hits something
+        return new Lava(this.reset, this.speed, this.reset);
+    } else {
+    // moving in the opposite direction
+        return new Lava(this.pos, this.speed.times(-1));
+    }
+}
+
+//Coin Updates (Wobble)
+
+const wobbleSpeed = 8, wobbleDist = 0.07
+Coin.prototype.update = function(time){
+    // wobble property is incremented to track time
+    let wobble = this.wobble + time *wobbleSpeed;
+    // wobble then used as an argument to Math.sin to 
+    // find the new position on the wave.
+    let wobblePos = Math.sin(Wobble) * wobbleDist;
+    return new Coin(this.basePos.plus(new Vec(0,wobblePos)),
+                    this.basePos, wobble);
+};
+
+
+//Player update
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+Player.prototype.update = function(time, state, keys){
+    let xSpeed = 0;
+    if (keys.ArrorLeft) xSpeed -= playerXSpeed;
+    if (keys.ArrorRight) xSpeed += playerXSpeed;
+    let pos = this.pos;
+    let movedX = pos.plus(new Vec(xSpeed * time, 0));
+    if (!state.level.touches(movedX,this.size, "wall")){
+        pos = movedX;
+    }
+
+    let ySpeed = this.speed.y + time * gravity;
+    let movedY = pos.plus(new Vec(0, ySpeed *time));
+    if (!state.level.touches(movedY, this.size, "Wall")){
+        pos = movedY;
+    } else if (keys.ArrowUp && ySpeed >0){
+        ySpeed = -jumpSpeed;
+    } else {
+        ySpeed = 0;
+    }
+    return new Player(pos, new Vec(xSpeed, ySpeed));
+}
+
+//Tracking keys
+
+function trackKeys(keys){
+    let down = Object.create(null);
+    function track(event) {
+        if (keys.includes(event.key)){
+            down[event.key] = event.type == "keydown";
+            //so that they don’t end up scrolling the page.
+            event.preventDefault();
+        }
+    }
+    window.addEventListener("keydown", track);
+    window.addEventListener("keyup", track)
+}
+const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"])
+
+
+//Running the game
+
+function runAnimation(framFunc) {
+    let lastTime = null;
+    function frame(time) {
+        if (lastTime != null){
+            let timeStep = Math.min(time - lastTime, 100) / 1000;
+            if (framFunc(timeStep) === false) return;
+        }
+        lastTime = time;
+        requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+function runLevel(level, Display){
+    let display = new Display(document.body,level);
+    let state = State.start(level);
+    let ending = 1;
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time,arrowKeys);
+            display.syncState(state);
+            if (state.tatus =="playing"){
+                return true;
+            } else if (ending > 0){
+                ending -= time;
+                return true;
+            } else {
+                display.clear();
+                resolve(state.status);
+                return false;
+            }
+        })
+    })
+}
+
+async function runGame(plans, Display){
+    for (let level = 0; level < plans.length;){
+        let status = await runLevel(new level(plans[level]), Display);
+        if (status == "won") level++;
+    }
+    console.log("You've won!")
 }
