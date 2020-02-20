cf3da0bd39498ac163da48c2ed537c578aa0a425
Peizhi Zheng <parisa1206@gmail.com>
2020-02-20 15:21:08 -0800

    skill sharing

--- cha21/skillsharing_server.js
+++ cha21/skillsharing_server.js
@@ -3,70 +3,71 @@ const Router = require("./router");
 const ecstatic = require("ecstatic");
 
 const router = new Router();
-const defaultHeaders = {"Content-Type":"text/plain"};
+const defaultHeaders = {"Content-Type": "text/plain"};
 
 class SkillShareServer {
     constructor(talks) {
-        this.talks = talks;
-        this.version = 0;
-        this.waiting = [];
+      this.talks = talks;
+      this.version = 0;
+      this.waiting = [];
 
-        let fileServer = ecstatic({root: "./public"});
-        this.server = createServer((request,response) => {
-            let resolved = router.resolve(this, request);
-            if (resolved){
-                resolved.catch(error => {
-                    if (error.status != null) return error;
-                    return {body: String(error), status:500};
-                }).then(({body, status =200, headers = defaultHeaders}) => {
-                    response.writeHead(status,headers);
-                    response.end(body);
-                });
-            } else {
-                fileServer(request, response);
-            }
-        })
-    }
-    start(port){
-        this.server.listen(port);
-    }
-    stop(){
-        this.server.close();
-    }
+    let fileServer = ecstatic({root: "./public"});
+    this.server = createServer((request, response) => {
+      let resolved = router.resolve(this, request);
+      if (resolved) {
+        resolved.catch(error => {
+          if (error.status != null) return error;
+          return {body: String(error), status: 500};
+        }).then(({body,
+                  status = 200,
+                  headers = defaultHeaders}) => {
+          response.writeHead(status, headers);
+          response.end(body);
+        });
+      } else {
+        fileServer(request, response);
+      }
+    });
+  }
+  start(port) {
+    this.server.listen(port);
+  }
+  stop() {
+    this.server.close();
+  }
 }
 
 //Talks as resources
 
-const talkPath = /^\/talks\/([^\/])$/
+const talkPath = /^\/talks\/([^\/]+)$/;
 
-router.add("GET", talkPath, async (server,title) => {
-    if (title in server.talks){
-        return {body: JSON.stringify(server.talks[title]),
-                headers:{"Content-Type": "application/json"}};
+router.add("GET", talkPath, async (server, title) => {
+    if (title in server.talks) {
+      return {body: JSON.stringify(server.talks[title]),
+              headers: {"Content-Type": "application/json"}};
     } else {
-        return {status:404, body: `No talk '${title} found'`};
+      return {status: 404, body: `No talk '${title}' found`};
     }
-})
+  });
 
-router.add("DELETE", talkPath, async (server,title) =>{
-    if ( title in server.talks){
-        delete server.talks[title];
-        server.updated();
+  router.add("DELETE", talkPath, async (server, title) => {
+    if (title in server.talks) {
+      delete server.talks[title];
+      server.updated();
     }
-    // No Content success status 
-    return {status:204};
-})
+    return {status: 204};
+  });
 
 //readStream, reads all content from a readable stream and 
 //returns a promise that resolves to a string.
-function readStream(stream){
-    return new Promise((resolve,reject) =>{
-        let data = "";
-        stream.on("error", reject);
-        stream.on("data", chunk => data += chunk.toString());
-        stream.on("end", () => resolve(data));
-    })
-}
+function readStream(stream) {
+    return new Promise((resolve, reject) => {
+      let data = "";
+      stream.on("error", reject);
+      stream.on("data", chunk => data += chunk.toString());
+      stream.on("end", () => resolve(data));
+    });
+  }
 
 //create new talks
 router.add("PUT", talkPath, async (server, title, request) => {
@@ -79,55 +80,55 @@ router.add("PUT", talkPath, async (server, title, request) => {
 //check whether the data it was given has presenter and summary properties, which are strings.
     if (!talk ||
         typeof talk.presenter != "string" ||
-        typeof talk.summary != "string"
-        ){
-    return {status:400, body:"Bad talk data"};
+        typeof talk.summary != "string") {
+    return {status: 400, body: "Bad talk data"};
     }
     server.talks[title] = {title,
                         presenter: talk.presenter,
                         summary: talk.summary,
-                        comments:[]};
+                        comments: []};
     server.updated();
-    return {status:204};
-})
+    return {status: 204};
+    });
 
-router.add("POST", /^\talks\/([^\/]+)\/comments$/, async (server, title, request) => {
-    let requestBody = await readStream(request);
-    let comment;
-    try {comment = JSON.parse(requestBody);} 
-    catch(_){
-        return {status:400, body: "Invalid JSON"};
-    }
-    if (!comment ||
+    router.add("POST", /^\/talks\/([^\/]+)\/comments$/,
+    async (server, title, request) => {
+        let requestBody = await readStream(request);
+        let comment;
+        try { comment = JSON.parse(requestBody); }
+        catch (_) { return {status: 400, body: "Invalid JSON"}; }
+
+        if (!comment ||
         typeof comment.author != "string" ||
-        typeof comment.message != "string"){
-            return {status:400, body:"Bad comment data"};
-    } else if (title in server.talks){
+        typeof comment.message != "string") {
+        return {status: 400, body: "Bad comment data"};
+        } else if (title in server.talks) {
         server.talks[title].comments.push(comment);
         server.updated();
-        return {status:204};
-    } else {
+        return {status: 204};
+        } else {
+        
         //Trying to add a comment to a nonexistent talk returns a 404 error.
         return {status:404, body: `No talk '${title} found`};
     }
 })
 
 //A helper method that builds up such an array and includes an ETag header in the response.
-SkillShareServer.prototype.talkResponse = function(){
+SkillShareServer.prototype.talkResponse = function() {
     let talks = [];
-    for (let title of Object.keys(this.talks)){
-        talks.push(this.talks[title]);
+    for (let title of Object.keys(this.talks)) {
+      talks.push(this.talks[title]);
     }
     return {
-        body:JSON.stringify(talks),
-        headers:{"Content-Type":"application/json",
-                "ETag":`"${this.version}"`}
-    }
-}
-router.add("GET", /^\/talks$/, async (server, request)=>{
+      body: JSON.stringify(talks),
+      headers: {"Content-Type": "application/json",
+                "ETag": `"${this.version}"`}
+    };
+  };
+  router.add("GET", /^\/talks$/, async (server, request) => {
     let tag = /"(.*)"/.exec(request.headers["if-none-match"]);
-    let wait = /\bwait=(\d+)/.exec(request.header["prefer"]);
-    if (!tag ||tag[1] !=server.version){
+    let wait = /\bwait=(\d+)/.exec(request.headers["prefer"]);
+    if (!tag || tag[1] != server.version) {
         return server.talkResponse();
     } else if (!wait){
         return {status:304};
